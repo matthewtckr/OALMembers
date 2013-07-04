a73c32473ccab167952daff10d190ce2b2154a90
Chase Patterson <chapatt@gmail.com>
2013-07-04 15:33:28 -0400

    Add various options, including font management and text formatting.

--- oalmembers.php
+++ oalmembers.php
@@ -12,10 +12,9 @@ License: Free To Use within Tipisa Lodge, Central Florida Council.  Contact Matt
 define("OALMEMBERS_PLUGIN_PATH", plugin_dir_path( __FILE__ ));
 define("OALMEMBERS_PLUGIN_URL", plugins_url( NULL, __FILE__ ));
 
-global $oalmembers_db_version, $oalmembers_table_name, $oalmembers_lodge;
+global $oalmembers_db_version, $oalmembers_table_name;
 $oalmembers_db_version = "1.0";
 $oalmembers_table_name = "oalmembers";
-$oalmembers_lodge = "Tipisa";
 
 function oalmembers_install() {
 	global $wpdb;
@@ -47,7 +46,12 @@ function oalmembers_install() {
 	if( ! $table_is_current )
 		update_option( "oalmembers_db_version", $oalmembers_db_version);
 	add_option( "oalmembers_last_update", 0);
-	add_option( "oalmembers_netami", 0);
+	add_option( "oalmembers_netami", NULL);
+	add_option( "oalmembers_lodge", NULL);
+	add_option( "oalmembers_style_name", array("HelveticaLTStd-Bold.otf","53",104,1057));
+	add_option( "oalmembers_style_title", array("HelveticaLTStd-Cond.otf","27",155,1013));
+	add_option( "oalmembers_style_netami", array("LinotypeZapfino Three.ttf","60", 290,907));
+	add_option( "oalmembers_style_expire", array("HelveticaLTStd-Roman.otf","33",749,897));
 }
 
 function oalmembers_uninstall() {
@@ -59,8 +63,17 @@ function oalmembers_uninstall() {
 	delete_option( "oalmembers_db_version");
 	delete_option( "oalmembers_last_update");
 	delete_option( "oalmembers_netami");
+	delete_option( "oalmembers_lodge");
+	delete_option( "oalmembers_style_name");
+	delete_option( "oalmembers_style_title");
+	delete_option( "oalmembers_style_netami");
+	delete_option( "oalmembers_style_expire");
 
 	unlink( OALMEMBERS_PLUGIN_PATH . "/card_template.png" );
+
+	oalmembers_delete_font("all");
+	
+	oalmembers_delete_cards();
 }
 
 function oalmembers_menu() {
@@ -78,8 +91,51 @@ function oalmembers_options() {
 			$records_updated = oalmembers_reload($oalm_roster);
 		if ($netami = $_POST['update_netami'])
 			$netami_updated = oalmembers_update_netami($netami);
+		if ($lodge = $_POST['update_lodge'])
+			$lodge_updated = oalmembers_update_lodge($lodge);
+		if ($delete_cards = $_POST['delete_cards'])
+			$cards_deleted = oalmembers_delete_cards();
 		if ($card_template = $_FILES['upload_card']['tmp_name'])
 			$card_updated = oalmembers_upload_card($card_template);
+
+		$style_name = array($_POST['font_name'], $_POST['size_name'], $_POST['x_name'], $_POST['y_name']);
+		if (($style_name == get_option(oalmembers_style_name)) ? FALSE : $style_name) {
+			$style_name_updated = oalmembers_update_style('name', $style_name);
+		}
+
+		$style_title = array($_POST['font_title'], $_POST['size_title'], $_POST['x_title'], $_POST['y_title']);
+		if (($style_title == get_option(oalmembers_style_title)) ? FALSE : $style_title) {
+			$style_title_updated = oalmembers_update_style('title', $style_title);
+		}
+
+		$style_netami = array($_POST['font_netami'], $_POST['size_netami'], $_POST['x_netami'], $_POST['y_netami']);
+		if (($style_netami == get_option(oalmembers_style_netami)) ? FALSE : $style_netami) {
+			$style_netami_updated = oalmembers_update_style('netami', $style_netami);
+		}
+
+		$style_expire = array($_POST['font_expire'], $_POST['size_expire'], $_POST['x_expire'], $_POST['y_expire']);
+		if (($style_expire == get_option(oalmembers_style_expire)) ? FALSE : $style_expire) {
+			$style_expire_updated = oalmembers_update_style('expire', $style_expire);
+		}
+
+		if ($font_dir_handle = opendir(OALMEMBERS_PLUGIN_PATH . "/fonts/")) {
+			$n = 0;
+			while (false !== ($entry = readdir($font_dir_handle))) {
+				if ($entry != "." && $entry != "..") {
+					if (isset($_POST['checkbox_' . $n]))
+						if ($_POST['delete_font_' . $n] == $entry) {
+							oalmembers_delete_font($entry);
+							$deleted_fonts[] = $entry;
+						}
+					$n++;
+				}
+			}
+		}
+
+		if ($font_file = $_FILES['upload_font']['tmp_name']) {
+			$font_name = $_FILES['upload_font']['name'];
+			$font_uploaded = oalmembers_upload_font($font_file, $font_name);
+		}
 	}
 	echo '<p>In OA Lodgemaster, create a CSV export file (field delimiter: ",", no field enclosures, line ending: "\r\n", and headers as listed after each field name below) that contains:';
 	echo '<ul>';
@@ -98,34 +154,171 @@ function oalmembers_options() {
 	echo '</ul></p>';
 	echo '<br />';
 	echo '<form id="oalmembers_upload" name="oalmembers_upload" method="post" action="" enctype="multipart/form-data" class="oalmembers_upload_form">';
-	echo '<fieldset name="upload_file" style="margin-bottom:10px;">';
+	echo '<fieldset name="upload_file" style="margin-bottom:20px;">';
 	echo '<p>Last Updated: ' . date('F j, Y g:i:s A', get_option( "oalmembers_last_update")) . '</p>';
 	echo '<label for="upload_file" style="margin-right:10px;">OALM Export:</label>';
 	echo '<input type="file" name="upload_file" accept=".csv" />';
 	echo '</fieldset>';
-	echo '<fieldset name="update_netami" style="margin-bottom:10px;">';
+
+	echo '<fieldset name="update_netami" style="margin-bottom:20px;">';
 	echo '<p>Current Netami Lekhiket: ' . get_option( "oalmembers_netami" ) . '</p>';
 	echo '<label for="update_netami" style="margin-right:10px;">Netami Lekhiket:</label>';
 	echo '<input type="text" name="update_netami" />';
 	echo '</fieldset>';
-	echo '<fieldset name="upload_card" style="margin-bottom:10px;">';
+
+	echo '<fieldset name="update_lodge" style="margin-bottom:20px;">';
+	echo '<p>Current Lodge: ' . get_option( "oalmembers_lodge" ) . '</p>';
+	echo '<label for="update_lodge" style="margin-right:10px;">Lodge:</label>';
+	echo '<input type="text" name="update_lodge" />';
+	echo '</fieldset>';
+
+	echo '<fieldset name="delete_cards" style="margin-bottom:20px;">';
+	echo '<label for="delete_cards" style="margin-right:10px;">Delete Generated Cards:</label>';
+	echo '<input type="checkbox" name="delete_cards" />';
+	echo '</fieldset>';
+
+	echo '<fieldset name="upload_card" style="margin-bottom:20px;">';
 	echo '<p><a href="' . OALMEMBERS_PLUGIN_URL . '/card_template.png" target="_blank">Current Membership Card Template</a></p>';
 	echo '<label for="upload_card" style="margin-right:10px;">Membership Card Template:</label>';
 	echo '<input type="file" name="upload_card" accept="image/*" />';
+	echo '</fieldset>';
 
+	echo '<fieldset name="manage_fonts" style="margin-bottom:20px;">';
 	if ($font_dir_handle = opendir(OALMEMBERS_PLUGIN_PATH . "/fonts/")) {
 		echo '<p>Fonts available:</p>';
-		echo '<ul>';
+		echo '<table>';
+		echo '<tr>';
+		echo '<td>Delete</td>';
+		echo '<td>&nbsp;</td>';
+		echo '</tr>';
 
+		$n = 0;
 		while (false !== ($entry = readdir($font_dir_handle))) {
 			if ($entry != "." && $entry != "..") {
-				echo '<li>' . $entry . '</li>';
+				echo '<tr>';
+				echo '<td><input type="checkbox" name="checkbox_' . $n . '" /></td>';
+				echo '<td><input type="hidden" name="delete_font_' . $n . '" value="' . $entry . '" /></td>';
+				echo '<td>' . $entry . '</td>';
+				echo '</tr>';
+				$n++;
 			}
 		}
 
+		echo '</table>';
+		closedir($font_dir_handle);
+	}
+	echo '<label for="upload_font" style="margin-right:10px;">Font file:</label>';
+	echo '<input type="file" name="upload_font" />';
+	echo '</fieldset>';
+	echo '<fieldset name="card_format" style="margin-bottom:20px;">';
+
+	echo '<p>Name:</p>';
+	echo '<label for="font_name" style="margin-right:10px;">Font:</label>';
+	echo '<select name="font_name" style="margin-right:10px;">';
+
+	if ($font_dir_handle = opendir(OALMEMBERS_PLUGIN_PATH . "/fonts/")) {
+		$n = 0;
+		while (false !== ($entry = readdir($font_dir_handle))) {
+			if ($entry != "." && $entry != "..") {
+				echo '<option '; 
+				if ($entry == get_option("oalmembers_style_name")[0])
+					echo 'selected="selected" ';
+				echo 'value="' . $entry . '" />' . $entry . '</option>';
+				$n++;
+			}
+		}
 		closedir($font_dir_handle);
 	}
 
+	echo '</select>';
+	echo '<label for="size_name" style="margin-right:10px;">Size:</label>';
+	echo '<input type="text" name="size_name" size="3" value="' . get_option("oalmembers_style_name")[1] . '" style="text-align:right; margin-right:10px;" />';
+	echo '<label for="x_name" style="margin-right:10px;">X:</label>';
+	echo '<input type="text" name="x_name" size="3" value="' . get_option("oalmembers_style_name")[2] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+	echo '<label for="y_name" style="margin-right:10px;">Y:</label>';
+	echo '<input type="text" name="y_name" size="3" value="' . get_option("oalmembers_style_name")[3] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+
+	echo '<p>Title:</p>';
+	echo '<label for="font_title" style="margin-right:10px;">Font:</label>';
+	echo '<select name="font_title" style="margin-right:10px;">';
+
+	if ($font_dir_handle = opendir(OALMEMBERS_PLUGIN_PATH . "/fonts/")) {
+		$n = 0;
+		while (false !== ($entry = readdir($font_dir_handle))) {
+			if ($entry != "." && $entry != "..") {
+				echo '<option '; 
+				if ($entry == get_option("oalmembers_style_title")[0])
+					echo 'selected="selected" ';
+				echo 'value="' . $entry . '" />' . $entry . '</option>';
+				$n++;
+			}
+		}
+		closedir($font_dir_handle);
+	}
+
+	echo '</select>';
+	echo '<label for="size_title" style="margin-right:10px;">Size:</label>';
+	echo '<input type="text" name="size_title" size="3" value="' . get_option("oalmembers_style_title")[1] . '" style="text-align:right; margin-right:10px;" />';
+	echo '<label for="x_title" style="margin-right:10px;">X:</label>';
+	echo '<input type="text" name="x_title" size="3" value="' . get_option("oalmembers_style_title")[2] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+	echo '<label for="y_title" style="margin-right:10px;">Y:</label>';
+	echo '<input type="text" name="y_title" size="3" value="' . get_option("oalmembers_style_title")[3] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+
+	echo '<p>Netami Lekhiket:</p>';
+	echo '<label for="font_netami" style="margin-right:10px;">Font:</label>';
+	echo '<select name="font_netami" style="margin-right:10px;">';
+
+	if ($font_dir_handle = opendir(OALMEMBERS_PLUGIN_PATH . "/fonts/")) {
+		$n = 0;
+		while (false !== ($entry = readdir($font_dir_handle))) {
+			if ($entry != "." && $entry != "..") {
+				echo '<option '; 
+				if ($entry == get_option("oalmembers_style_netami")[0])
+					echo 'selected="selected" ';
+				echo 'value="' . $entry . '" />' . $entry . '</option>';
+				$n++;
+			}
+		}
+		closedir($font_dir_handle);
+	}
+
+	echo '</select>';
+	echo '<label for="size_netami" style="margin-right:10px;">Size:</label>';
+	echo '<input type="text" name="size_netami" size="3" value="' . get_option("oalmembers_style_netami")[1] . '" style="text-align:right; margin-right:10px;" />';
+	echo '<label for="x_netami" style="margin-right:10px;">X:</label>';
+	echo '<input type="text" name="x_netami" size="3" value="' . get_option("oalmembers_style_netami")[2] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+	echo '<label for="y_netami" style="margin-right:10px;">Y:</label>';
+	echo '<input type="text" name="y_netami" size="3" value="' . get_option("oalmembers_style_netami")[3] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+
+	echo '<p>Expiration Date:</p>';
+	echo '<label for="font_expire" style="margin-right:10px;">Font:</label>';
+	echo '<select name="font_expire" style="margin-right:10px;">';
+
+	if ($font_dir_handle = opendir(OALMEMBERS_PLUGIN_PATH . "/fonts/")) {
+		$n = 0;
+		while (false !== ($entry = readdir($font_dir_handle))) {
+			if ($entry != "." && $entry != "..") {
+				echo '<option '; 
+				if ($entry == get_option("oalmembers_style_expire")[0])
+					echo 'selected="selected" ';
+				echo 'value="' . $entry . '" />' . $entry . '</option>';
+				$n++;
+			}
+		}
+		closedir($font_dir_handle);
+	}
+
+	echo '</select>';
+	echo '<label for="size_expire" style="margin-right:10px;">Size:</label>';
+	echo '<input type="text" name="size_expire" size="3" value="' . get_option("oalmembers_style_expire")[1] . '" style="text-align:right; margin-right:10px;" />';
+	echo '<label for="x_expire" style="margin-right:10px;">X:</label>';
+	echo '<input type="text" name="x_expire" size="3" value="' . get_option("oalmembers_style_expire")[2] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+	echo '<label for="y_expire" style="margin-right:10px;">Y:</label>';
+	echo '<input type="text" name="y_expire" size="3" value="' . get_option("oalmembers_style_expire")[3] . '" style="text-align:right;" /><span style="margin-right:10px;">px</span>';
+
+	$card_url = oalmembers_generate_card(TRUE, "First", "Lastname", "Jr.", "Level", "Chaptername", strtotime(date('Y') . "-12-31"), "0000000");
+	echo '<p>Preview:</p>';
+	echo '<img class="oalmembers_print" src="' . $card_url . '" style="width:3.53in;" />';
 	echo '</fieldset>';
 	echo '<input type="submit" name="upload" values="Upload" />';
 	echo '</form>';
@@ -133,8 +326,25 @@ function oalmembers_options() {
 		echo "<p>Imported $records_updated records.</p>";
 	if(isset($netami_updated))
 		echo '<p>Updated Netami Lekhiket to ' . $netami_updated . '.</p>';
+	if(isset($lodge_updated))
+		echo '<p>Updated Lodge to ' . $lodge_updated . '.</p>';
+	if(isset($cards_deleted))
+		echo '<p>Cards deleted.</p>';
 	if(isset($card_updated))
 		echo '<p>Membership Card Template Updated: <a href="' . $card_updated . '" target="_blank">New Membership Card Template</a> (image may not appear to have changed due to browser caching)</p>';
+	if(isset($style_name_updated))
+		echo '<p>Name Style Updated</p>';
+	if(isset($style_title_updated))
+		echo '<p>Title Style Updated</p>';
+	if(isset($style_netami_updated))
+		echo '<p>Netami Style Updated</p>';
+	if(isset($style_expire_updated))
+		echo '<p>Expire Style Updated</p>';
+	if(isset($deleted_fonts))
+		foreach ($deleted_fonts as $font)
+			echo '<p>Deleted "' . $font . '"</p>';
+	if(isset($font_uploaded))
+		echo '<p>Font "' . $font_uploaded . '" uploaded</p>';
 	echo '</div>';
 }
 
@@ -185,13 +395,24 @@ function oalmembers_reload($file) {
 		$rows_imported++;
 	}
 	update_option( "oalmembers_last_update", current_time('timestamp', 0));
+	oalmembers_delete_cards();
 	return $rows_imported;
-
 }
 
 function oalmembers_update_netami($netami) {
 	update_option( "oalmembers_netami", $netami);
+
+	oalmembers_delete_cards();
+
 	return $netami;
+}
+
+function oalmembers_update_lodge($lodge) {
+	update_option( "oalmembers_lodge", $lodge);
+
+	oalmembers_delete_cards();
+
+	return $lodge;
 }
 
 function oalmembers_upload_card($file) {
@@ -199,38 +420,54 @@ function oalmembers_upload_card($file) {
 	move_uploaded_file( $file, $target ); 
 	$template_url = OALMEMBERS_PLUGIN_URL . '/card_template.png';
 
+	oalmembers_delete_cards();
+
 	return $template_url;
 }
 
-function oalmembers_generate_card($firstname, $lastname, $suffix, $level, $chapter, $expire, $oalmid) {
-	global $oalmembers_lodge;
+function oalmembers_upload_font($file, $name) {
+	$target = OALMEMBERS_PLUGIN_PATH . "/fonts/" . $name;
+	move_uploaded_file( $file, $target ); 
+
+	return $name;
+}
+
+function oalmembers_generate_card($overwrite, $firstname, $lastname, $suffix, $level, $chapter, $expire, $oalmid) {
+	get_option( "oalmembers_lodge" );
 	$netami = get_option( "oalmembers_netami" );
 	$template = OALMEMBERS_PLUGIN_PATH . "/card_template.png";
 	$font_dir = OALMEMBERS_PLUGIN_PATH . "/fonts/";
 
+	$style_name = get_option("oalmembers_style_name");
 	$name_opt = array(
-		"font" => $font_dir . 'HelveticaLTStd-Bold.otf',
-		"size" => '53',
-		"x-coord" => 104,
-		"y-coord" => 1057,
+		"font" => $font_dir . $style_name[0],
+		"size" => $style_name[1],
+		"x-coord" => $style_name[2],
+		"y-coord" => $style_name[3],
 	);
+
+	$style_title = get_option("oalmembers_style_title");
 	$title_opt = array(
-		"font" => $font_dir . 'HelveticaLTStd-Cond.otf',
-		"size" => 27,
-		"x-coord" => 155,
-		"y-coord" => 1013,
+		"font" => $font_dir . $style_title[0],
+		"size" => $style_title[1],
+		"x-coord" => $style_title[2],
+		"y-coord" => $style_title[3],
 	);
+
+	$style_netami = get_option("oalmembers_style_netami");
 	$netami_opt = array(
-		"font" => $font_dir . 'LinotypeZapfino Three.ttf',
-		"size" => 60,
-		"x-coord" => 290,
-		"y-coord" => 907,
+		"font" => $font_dir . $style_netami[0],
+		"size" => $style_netami[1],
+		"x-coord" => $style_netami[2],
+		"y-coord" => $style_netami[3],
 	);
+
+	$style_expire = get_option("oalmembers_style_expire");
 	$expire_opt = array(
-		"font" => $font_dir . 'HelveticaLTStd-Roman.otf',
-		"size" => 33,
-		"x-coord" => 749,
-		"y-coord" => 897,
+		"font" => $font_dir . $style_expire[0],
+		"size" => $style_expire[1],
+		"x-coord" => $style_expire[2],
+		"y-coord" => $style_expire[3],
 	);
 
 	if($level == "Ordeal") {
@@ -249,7 +486,7 @@ function oalmembers_generate_card($firstname, $lastname, $suffix, $level, $chapt
 	$full_path = $path . $filename;
 	$card_url = OALMEMBERS_PLUGIN_URL . "/generated/" . $filename;
 
-	if(!(file_exists($full_path) && is_file($full_path))) {
+	if(file_exists($template) && ($overwrite == TRUE || (!(file_exists($full_path) && is_file($full_path)) && $overwrite == FALSE))) {
 		$image = new Imagick($template);
 		$height = $image->getimageheight();
 
@@ -263,7 +500,7 @@ function oalmembers_generate_card($firstname, $lastname, $suffix, $level, $chapt
 		$draw_title->setFillColor('#000000');
 		$draw_title->setFont($title_opt['font']);
 		$draw_title->setFontSize($title_opt['size']);
-		$image->annotateImage($draw_title,$title_opt['x-coord'],$height-$title_opt['y-coord'],0,"is " . $level_article . " " . $level . $level_suffix . " member in\n" . $chapter . " Chapter, " . $oalmembers_lodge . " Lodge");
+		$image->annotateImage($draw_title,$title_opt['x-coord'],$height-$title_opt['y-coord'],0,"is " . $level_article . " " . $level . $level_suffix . " member in\n" . $chapter . " Chapter, " . get_option( "oalmembers_lodge" ) . " Lodge");
 
 		$draw_netami = new ImagickDraw();
 		$draw_netami->setFillColor('#000000');
@@ -283,6 +520,39 @@ function oalmembers_generate_card($firstname, $lastname, $suffix, $level, $chapt
 	}
 
 	return $card_url;
+}
+
+function oalmembers_update_style($block,$style) {
+	update_option('oalmembers_style_' . $block, $style);
+
+	return 0;
+}
+
+function oalmembers_delete_font($font_name) {
+	if($font_name == "all") {
+		$files = glob(OALMEMBERS_PLUGIN_PATH . "fonts/*"); // get all file names
+		foreach($files as $file){ // iterate files
+			if(is_file($file))
+				unlink($file); // delete file
+		}
+		rmdir(OALMEMBERS_PLUGIN_PATH . "fonts");
+	}
+	else {
+		unlink( OALMEMBERS_PLUGIN_PATH . "fonts/" . $font_name );
+	}
+
+	return 0;
+}
+
+function oalmembers_delete_cards() {
+	$files = glob(OALMEMBERS_PLUGIN_PATH . "generated/*"); // get all file names
+	foreach($files as $file){ // iterate files
+		if(is_file($file))
+			unlink($file); // delete file
+	}
+	rmdir(OALMEMBERS_PLUGIN_PATH . "generated");
+
+	return 0;
 }
 
 function oalmembers_lookup_record() {
@@ -366,7 +636,7 @@ function oalmembers_lookup_record() {
 			$html .= "</tr>";
 			$html .= "</table>";
 
-			$card_url = oalmembers_generate_card($firstname, $lastname, $suffix, $level, $chapter, $dues, $oalmid);
+			$card_url = oalmembers_generate_card(FALSE, $firstname, $lastname, $suffix, $level, $chapter, $dues, $oalmid);
 			$html .= '<img class="oalmembers_print" src="' . $card_url . '" style="width:3.53in;" />';
 			$html .= "<p><a href='javascript:window.print()'>Print Memebership Card</a></p>";
 			$html .= "</div>";
